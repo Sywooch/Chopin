2a9c8069d6462222d6243d6ad3b57da568e5516a
Marcelo Briones <ing@marcelobriones.com.ar>
2015-04-26 21:04:17 -0300

    Set up code structure

---
+++ config/db.sample.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'class' => 'yii\db\Connection',
+  'dsn' => 'mysql:host=localhost;dbname=chopin_development',
+  'username' => 'root',
+  'password' => '',
+  'charset' => 'utf8',
+];
