c504041d4e69aa155115461275a1036e771b5a9c
Marcelo Briones <ing@marcelobriones.com.ar>
2015-05-20 01:16:03 -0300

    Correct migration behaviour for a new dev box

--- migrations/m150501_162800_rewards_to_integers.php
+++ migrations/m150501_162800_rewards_to_integers.php
@@ -6,13 +6,13 @@ use yii\db\Migration;
 class m150501_162800_rewards_to_integers extends Migration {
 
     public function safeUp() {
-        $this->alterColumn('achievement', 'reward', Schema::TYPE_INTEGER);
-        $this->alterColumn('person_achievement', 'reward', Schema::TYPE_INTEGER);
+        $this->alterColumn('{{%achievement}}', 'reward', Schema::TYPE_INTEGER);
+        $this->alterColumn('{{%person_achievement}}', 'reward', Schema::TYPE_INTEGER);
     }
 
     public function safeDown() {
-        $this->alterColumn('achievement', 'reward', Schema::TYPE_MONEY);
-        $this->alterColumn('person_achievement', 'reward', Schema::TYPE_MONEY);
+        $this->alterColumn('{{%achievement}}', 'reward', Schema::TYPE_MONEY);
+        $this->alterColumn('{{%person_achievement}}', 'reward', Schema::TYPE_MONEY);
     }
 
 }
